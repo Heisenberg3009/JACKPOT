5b4a50250aaa80bc0e7d7d70c3cd280ee2f543a7
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2024-10-31 11:26:42 +0530

    Second Commit

---
+++ project.js
@@ -0,0 +1,11 @@
+/*
+
+Step 1: Initialize nodejs using npm init.
+
+Step 2: Install Prompt Sync package by using: npm i prompt -sync
+The command npm i prompt-sync is used to install the prompt-sync package, 
+which is a Node.js module that enables synchronous user input (prompting) in the 
+terminal. In other words, it allows you to ask users for input and wait for their 
+response without needing complex asynchronous code.
+
+*/
